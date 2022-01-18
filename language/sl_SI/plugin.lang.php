b777ed4dfb5cd56704d2592dc2cd90196cb96b57
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-18 11:57:30 +0100

    sl_SI
    Slovenian language

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+
+$lang['StereoZoom'] = 'StereoZoom';
+
+// home
+$lang['What StereoZoom can do for me?'] = 'Kaj lahko StereoZoom naredi zame?';
+
+//Header
+$lang['Welcome'] = 'Dobrodošli';
+$lang['Help'] = 'Pomoč';
+$lang['Configuration'] = 'Konfiguracija';
+
+// help.tpl
+$lang['Keyboard usage and shortcuts'] = 'Uporaba tipkovnice in bližnjice';
+
+// config.tpl
+$lang['How to detect stereogram picture?'] = 'Kako zaznati stereogramsko sliko?';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'Ime datoteke običajne slike: sample.jpg ali 20180101T123059_dscn9876.jpg';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'Ime datoteke slike stereograma: sample%s%s%s.jpg ali 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram detection'] = 'Zaznavanje stereograma';
+$lang['Define your suffix:'] = 'Določite svojo pripono:';
+$lang['for instance: _rl, -lr, stereo, ...'] = 'na primer: _rl, -lr, stereo, ...';
+$lang['a possible dash followed by from 2 to 6 characters'] = 'možen pomišljaj, ki mu sledi od 2 do 6 znakov';
+$lang['Stereogram view type'] = 'Vrsta pogleda stereograma';
+$lang['Cross view'] = 'Prekrižani pogled';
+$lang['Parallel view'] = 'Vzporedni pogled';
+$lang['Stereogram zoom'] = 'Stereogramski zoom (povečava)';
+$lang['Mouse wheel zoom strength:'] = 'Moč povečave miškinega kolesca:';
+$lang['Save Settings'] = 'Shrani nastavitve';
+
+// picture_sz100.tpl
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = 'Povečanje 100 % / Prilagoditev povečave&#013;[Z] tipka';
+$lang['Zoom +&#013;[+] key'] = 'Tipka povečave +&#013;[+]';
+$lang['Zoom −&#013;[−] key'] = 'Tipka za povečavo -&#013;[-]';
+$lang['Reset&#013;[R] key'] = 'Ponastavitev&#013;[R] tipka';
+$lang['Full screen&#013;[F] key'] = 'Celozaslonski zaslon&#013;[F] tipka';
+$lang['Full window&#013;[W] key'] = 'Celotno okno&#013;[W] tipka';
+$lang['Help&#013;[?] key'] = 'Pomoč&#013;[?] tipka';
+$lang['Switch right and left views&#013;[X] key'] = 'Preklop med desnim in levim pogledom&#013;[X] tipka';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'Premakni: oba pogleda se premakneta z miško&#013;Prilagodi: en pogled se premakne z miško&#013;[Ctrl] tipka + miška';
+
+$lang['double click : previous'] = 'dvojni klik : prejšnja';
+$lang['double click : next'] = 'dvojni klik : naprej';
+$lang['drag : move&#013;double click : previous'] = 'povlecite : premaknite&#013;dvojni klik : prejšnja';
+$lang['drag : move&#013;double click : next'] = 'povlecite : premaknite&#013;dvojni klik : naprej';
+
+$lang['Z Fit'] = 'Z Fit';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Move'] = 'Premik';
+$lang['Adjust'] = 'Prilagodi';
+$lang['Please wait'] = 'Prosimo počakajte';
+
+
+
+// $lang[''] = '';
